e2714e988e0233d870cf034221f7a560e748bb6c
NARRA LEKHAN KUMAR <lekhannarra2003@gmail.com>
2024-09-29 17:37:32 +0530

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var options = {
+    strings: ["Frontend Developer", " Web Designer", "UI/UX Designer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  };
+
+  var typed = new Typed(".text", options);
+});
